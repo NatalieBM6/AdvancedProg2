7a523538cf07ff569b3230612219555d51bf4df2
NatalieBM6 <33790319+NatalieBM6@users.noreply.github.com>
2018-04-10 02:55:17 +0300

    Changed the main function.

--- ImageService/ImageService/Program.cs
+++ ImageService/ImageService/Program.cs
@@ -14,21 +14,12 @@ namespace ImageService
         /// </summary>
         static void Main(string[] args)
         {
-            ServiceBase[] ServicesToRun = new ServiceBase[] { new ImageService(args) };
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ImageService()
+            };
             ServiceBase.Run(ServicesToRun);
         }
     }
-}using System;
-
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class Class1
-{
-	public Class1()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
 }
